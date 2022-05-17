25bc72d9fbddea650589076e07847bf29d691d73
Ron <ron.martin@posteo.net>
2022-05-17 21:57:28 +0200

    Implemented replacement of tokens in template file

--- src/preProcessor/preProcessor.py
+++ src/preProcessor/preProcessor.py
@@ -24,20 +24,37 @@ class PreProcessor:
 
         print('\tPreProcessor done')
 
-
     def __createSimulationFolders(self):
         for testNr in range(self.experimentalPlan.nrTests):
             pathToTestNrFolder = self.config.pathToSimulationFolder + '/' + str(testNr)
             os.makedirs(pathToTestNrFolder, exist_ok=True)
-            self.__copyTemplatesToSimulationFolders(pathToTestNrFolder)
+            self.__copyTemplatesToSimulationFolders(pathToTestNrFolder, testNr)
         print('\t\tFolder created, templates copied and tokens replaced: %s'%self.config.pathToSimulationFolder)
 
-    def __copyTemplatesToSimulationFolders(self, pathToTestNrFolder: str):
+    def __copyTemplatesToSimulationFolders(self, pathToTestNrFolder: str, testNr:int):
         shutil.copytree(self.config.pathToTemplateFolder, pathToTestNrFolder,
                         dirs_exist_ok=True)
-        self.__replaceTemplateTokensInSimulationFolder(pathToTestNrFolder)
+        self.__replaceTemplateTokensInSimulationFolder(pathToTestNrFolder, testNr)
+
+    def __replaceTemplateTokensInSimulationFolder(self, pathToTestNrFolder: str, testNr:int):
+        # loop through all files in sim folder
+        for file in os.listdir(pathToTestNrFolder):
+            # replace all tokens with factor value of current test nr
+            if '.tmpl' in file:
+                pathToTemplateFile = pathToTestNrFolder + '/' + file
+                with open(pathToTemplateFile, 'r') as templateFile:
+                    templateFileString = templateFile.read()
+                    for factor in self.experimentalPlan.factorList:
+                        replaceFactorValue = str(self.experimentalPlan.factorPlan.at[testNr, factor])
+                        templateFileString = templateFileString.replace('$%s$'%factor, '%s'%replaceFactorValue)
+
+                # write the new string with replaced values to a new file without .tmpl ending
+                with open(pathToTemplateFile[:-5], 'w') as valueFile:
+                    valueFile.write(templateFileString)
+
+                # delete template file
+                os.remove(pathToTemplateFile)
 
 
-    def __replaceTemplateTokensInSimulationFolder(self, pathToTestNrFolder: str):
-        pass
+
 
